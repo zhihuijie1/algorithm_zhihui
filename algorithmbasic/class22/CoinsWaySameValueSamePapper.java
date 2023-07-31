ecc638eec6a2e9508460b0810b32889c797176c3
yibai <404223879@qq.com>
2023-07-31 20:11:32 +0800

    /**  * arr是货币数组，其中的值都是正数。再给定一个正数aim。  * 每个值都认为是一张货币，  * 认为值相同的货币没有任何不同，  * 返回组成aim的方法数  * 例如：arr = {1,2,1,1,2,1,2}，aim = 4  * 方法：1+1+1+1、1+1+2、2+2  * 一共就3种方法，所以返回3  */

--- algorithmbasic/class22/CoinsWaySameValueSamePapper.java
+++ algorithmbasic/class22/CoinsWaySameValueSamePapper.java
@@ -39,8 +39,9 @@ public class CoinsWaySameValueSamePapper {
         int[] zhangs = new int[map.size()];
         int index = 0;
         for (Map.Entry<Integer, Integer> entry : map.entrySet()) {
-            coins[index++] = entry.getKey();
-            zhangs[index++] = entry.getValue();
+            coins[index] = entry.getKey();
+            zhangs[index] = entry.getValue();
+            index++;
         }
         return new Info(coins, zhangs);
     }
@@ -73,8 +74,8 @@ public class CoinsWaySameValueSamePapper {
         if (arr == null || arr.length == 0 || aim < 0) {
             return 0;
         }
-        int N = arr.length;
         Info info = createInfo(arr);
+        int N = info.coins.length;
         int[] coins = info.coins;
         int[] zhangs = info.zhangs;
         int[][] dp = new int[aim + 1][N + 1];
@@ -84,8 +85,8 @@ public class CoinsWaySameValueSamePapper {
         for (int L = N - 1; L >= 0; L--) {
             for (int H = 0; H <= aim; H++) {
                 int ways = 0;
-                for (int zhang = 0; zhang <= zhangs[L] && aim - coins[L] * zhang >= 0; zhang++) {
-                    ways += dp[aim - zhang * coins[L]][L + 1];
+                for (int zhang = 0; H - coins[L] * zhang >= 0 && zhang <= zhangs[L]; zhang++) {
+                    ways += dp[H - zhang * coins[L]][L + 1];
                 }
                 dp[H][L] = ways;
             }
@@ -93,5 +94,73 @@ public class CoinsWaySameValueSamePapper {
         return dp[aim][0];
     }
 
+    //dp方法优化
+    public static int dp2(int[] arr, int aim) {
+        if (arr == null || arr.length == 0 || aim < 0) {
+            return 0;
+        }
+        Info info = createInfo(arr);
+        int N = info.coins.length;
+        int[] coins = info.coins;
+        int[] zhangs = info.zhangs;
+        int[][] dp = new int[aim + 1][N + 1];
+        dp[0][N] = 1;
+        //当前位置到底拿几张。
+        for (int L = N - 1; L >= 0; L--) {
+            for (int H = 0; H <= aim; H++) {
+                int key = dp[H][L + 1];
+                if (H - coins[L] >= 0) {
+                    key += dp[H - coins[L]][L];
+                }
+                if (H - coins[L] - zhangs[L] * coins[L] >= 0) {
+                    key -= dp[H - coins[L] - zhangs[L] * coins[L]][L + 1];
+                }
+                dp[H][L] = key;
+            }
+        }
+        return dp[aim][0];
+    }
 
+    // 为了测试
+    public static int[] randomArray(int maxLen, int maxValue) {
+        int N = (int) (Math.random() * maxLen);
+        int[] arr = new int[N];
+        for (int i = 0; i < N; i++) {
+            arr[i] = (int) (Math.random() * maxValue) + 1;
+        }
+        return arr;
+    }
+
+    // 为了测试
+    public static void printArray(int[] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.println();
+    }
+
+    // 为了测试
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 20;
+        int testTime = 1000000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = randomArray(maxLen, maxValue);
+            int aim = (int) (Math.random() * maxValue);
+            int ans1 = coinsWay(arr, aim);
+            int ans2 = dp(arr, aim);
+            int ans3 = dp2(arr, aim);
+            if (ans1 != ans2 || ans1 != ans3) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println(aim);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                System.out.println(ans3);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+    }
 }
