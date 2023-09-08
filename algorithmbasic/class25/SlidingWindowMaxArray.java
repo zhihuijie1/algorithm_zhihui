3220eb31f35e8d165a99d20a231cf330a3b96519
yibai <404223879@qq.com>
2023-09-08 17:58:45 +0800

    窗口内最大值或最小值更新结构的实现

--- algorithmbasic/class25/SlidingWindowMaxArray.java
+++ algorithmbasic/class25/SlidingWindowMaxArray.java
@@ -1,5 +1,6 @@
 package algorithmbasic.class25;
 
+import java.util.Arrays;
 import java.util.LinkedList;
 
 /**
@@ -14,11 +15,11 @@ public class SlidingWindowMaxArray {
     //(L,R]
     public static int[] getMaxWindow(int[] arr, int w) {
         //过滤非法条件
-        if(arr == null || arr.length <= 0 || w < 0) {
+        if (arr == null || arr.length <= w || w < 1) {
             return null;
         }
         //创建存储返回值的数组
-            //结果数目
+        //结果数目
         int nub = arr.length - w + 1;
         int[] res = new int[nub];
         int index = 0;//为res服务
@@ -29,29 +30,105 @@ public class SlidingWindowMaxArray {
         // [4,3,5,4,3,3,6,7]
         //L R
         for (int R = 0; R < arr.length; R++) {
-            while(!list.isEmpty() && list.peekLast() <= arr[R]) { //peelLast --> 双端队列最后一个节点
+            while (!list.isEmpty() && arr[list.peekLast()] <= arr[R]) { //peelLast --> 双端队列最后一个节点
                 list.pollLast();
             }
             //尾插下标
             list.addLast(R);
-
             //弹出窗口头部节点,该弹出的节点 -> R - W
-            while(list.peekFirst() <= R - w) {
+            while (!list.isEmpty() && list.peekFirst() <= R - w) {
                 list.pollFirst();
             }
             //存放最大值
-            res[index++] = list.peekFirst();
+            if (R - w + 1 >= 0) { //只有形成了窗口你才可以进行最大值的存储。
+                res[index++] = arr[list.peekFirst()];
+            }
         }
         return res;
     }
-
-
-
-
     //4,3,5,4,3,3,6,7
     //0,1,2,3,4,5,6,7
     //w=3
     //R=6
+    //555467
 
+    // --------------------------------------------------------------------------------------
+    // for test
+    public static int[] generateRandomArray(int maxSize, int maxValue) {
+        int[] arr = new int[(int) ((maxSize + 1) * Math.random())];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
 
+    // for test
+    public static boolean isEqual(int[] arr1, int[] arr2) {
+        if ((arr1 == null && arr2 != null) || (arr1 != null && arr2 == null)) {
+            return false;
+        }
+        if (arr1 == null && arr2 == null) {
+            return true;
+        }
+        if (arr1.length != arr2.length) {
+            return false;
+        }
+        for (int i = 0; i < arr1.length; i++) {
+            if (arr1[i] != arr2[i]) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    // 暴力的对数器方法
+    public static int[] right(int[] arr, int w) {
+        if (arr == null || w < 1 || arr.length < w) {
+            return null;
+        }
+        int N = arr.length;
+        int[] res = new int[N - w + 1];
+        int index = 0;
+        int L = 0;
+        int R = w - 1;
+        while (R < N) {
+            int max = arr[L];
+            for (int i = L + 1; i <= R; i++) {
+                max = Math.max(max, arr[i]);
+
+            }
+            res[index++] = max;
+            L++;
+            R++;
+        }
+        return res;
+    }
+
+/*    public static void main(String[] args) {
+        int testTime = 100000;
+        int maxSize = 10;
+        int maxValue = 10;
+        //System.out.println("test begin");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxSize, maxValue);
+            //System.out.println(Arrays.toString(arr));
+            int w = (int) (Math.random() * (arr.length + 1));
+            int[] ans1 = getMaxWindow(arr, w);
+            int[] ans2 = right(arr, w);
+            if (!isEqual(ans1, ans2)) {
+                System.out.println(Arrays.toString(arr));
+                System.out.println("Oops!");
+                break;
+            }
+        }
+        System.out.println("test finish");
+    }*/
+
+    public static void main(String[] args) {
+        //int[] arr = {4,3,5,4,3,3,6,7};
+        int[] arr = {9, 0, 7, 7, 5, 0, 4};
+        int w = 3;
+        int[] res = getMaxWindow(arr,w);
+        System.out.println(Arrays.toString(res));
+    }
 }
