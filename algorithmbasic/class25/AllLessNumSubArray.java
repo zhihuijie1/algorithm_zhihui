d3b46ecdcf0b97938fe2da064ead4d80d9913c38
yibai <404223879@qq.com>
2023-09-11 17:52:36 +0800

    达标数组：sub中最大值 – sub中最小值 <= num

--- algorithmbasic/class25/AllLessNumSubArray.java
+++ algorithmbasic/class25/AllLessNumSubArray.java
@@ -9,37 +9,24 @@ import java.util.LinkedList;
  */
 public class AllLessNumSubArray {
     public static int num(int[] arr, int sum) {
-
-        /*双端队列 大 --> 小*/
-        LinkedList<Integer> deque = new LinkedList<>();
-        /*1：通过滑动窗口来锁定范围*/
-        for (int L = -1; L < arr.length - 1; L++) {
-            for (int R = L; R < arr.length; R++) {
-                /*(L , R]就是窗口的范围*/
-                /*2：在窗口内进行寻找最大值与最小值*/
-                if (arr[R] > arr[R - 1]) {
-                    int beforeIndex = R - 1;
-                    while (arr[beforeIndex] <= arr[beforeIndex + 1]) {
-                        swap(deque, beforeIndex, beforeIndex + 1);
-                        beforeIndex--;
-                    }
-                } else {
-                    deque.addLast(R);
-                }
-
-                while (!deque.isEmpty() && deque.peekFirst() <= L) {
-                    deque.pollFirst();
-                }
-
-            }
+        if(arr == null || arr.length == 0 || sum < 0) {
+            return -1;
         }
+        LinkedList<Integer> maxWindow = new LinkedList<>();
+        LinkedList<Integer> minWindow = new LinkedList<>();
+        int L = 0;
+        for (int R = 0; R < arr.length; R++) {
+            while(arr[R] > maxWindow.peekLast()) {
+                maxWindow.pollLast();
+            }
+            maxWindow.addLast(R);
 
-        /*3：sub中最大值 – sub中最小值 <= num count++*/
-    }
+            while(arr[R] < minWindow.peekLast()) {
+                minWindow.pollLast();
+            }
+            minWindow.addLast(R);
 
-    public static void swap(LinkedList<> arr, int index1, int index2) {
-        int temp = arr[index1];
-        arr[index1] = arr[index2];
-        arr[index2] = temp;
+
+        }
     }
 }
