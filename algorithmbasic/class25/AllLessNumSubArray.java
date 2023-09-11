89eeac12307741ac6690100a511bcc2644219a56
yibai <404223879@qq.com>
2023-09-11 19:15:11 +0800

    达标数组：sub中最大值 – sub中最小值 <= num

--- algorithmbasic/class25/AllLessNumSubArray.java
+++ algorithmbasic/class25/AllLessNumSubArray.java
@@ -9,24 +9,111 @@ import java.util.LinkedList;
  */
 public class AllLessNumSubArray {
     public static int num(int[] arr, int sum) {
-        if(arr == null || arr.length == 0 || sum < 0) {
-            return -1;
+        if (arr == null || arr.length == 0 || sum < 0) {
+            return 0;
         }
+
+        int count = 0;
         LinkedList<Integer> maxWindow = new LinkedList<>();
         LinkedList<Integer> minWindow = new LinkedList<>();
-        int L = 0;
-        for (int R = 0; R < arr.length; R++) {
-            while(arr[R] > maxWindow.peekLast()) {
-                maxWindow.pollLast();
+
+        //[L,R]
+        int R = 0;
+        for (int L = 0; L < arr.length; L++) {
+            //R不回退
+            while (R < arr.length) {
+                //维护大端双端队列
+                while (!maxWindow.isEmpty() && arr[R] > maxWindow.peekLast()) {
+                    maxWindow.pollLast();
+                }
+                maxWindow.addLast(R);
+
+                //维护小端双端队列
+                while (!minWindow.isEmpty() && arr[R] < minWindow.peekLast()) {
+                    minWindow.pollLast();
+                }
+                minWindow.addLast(R);
+
+                //进行条件判断
+                if (arr[maxWindow.peekFirst()] - arr[minWindow.peekFirst()] > sum) {
+                    count += R - L;
+                    while (!maxWindow.isEmpty() && maxWindow.peekFirst() <= L) {
+                        maxWindow.pollFirst();
+                    }
+                    while (!minWindow.isEmpty() && minWindow.peekFirst() <= L) {
+                        minWindow.pollFirst();
+                    }
+                    break;
+                } else {
+                    R++;
+                }
             }
-            maxWindow.addLast(R);
-
-            while(arr[R] < minWindow.peekLast()) {
-                minWindow.pollLast();
-            }
-            minWindow.addLast(R);
-
-
         }
+        return count;
+    }
+
+    public static int right(int[] arr, int sum) {
+        if (arr == null || arr.length == 0 || sum < 0) {
+            return 0;
+        }
+        int N = arr.length;
+        int count = 0;
+        for (int L = 0; L < N; L++) {
+            for (int R = L; R < N; R++) {
+                int max = arr[L];
+                int min = arr[L];
+                for (int i = L + 1; i <= R; i++) {
+                    max = Math.max(max, arr[i]);
+                    min = Math.min(min, arr[i]);
+                }
+                if (max - min <= sum) {
+                    count++;
+                }
+            }
+        }
+        return count;
+    }
+
+    // for test
+    public static int[] generateRandomArray(int maxLen, int maxValue) {
+        int len = (int) (Math.random() * (maxLen + 1));
+        int[] arr = new int[len];
+        for (int i = 0; i < len; i++) {
+            arr[i] = (int) (Math.random() * (maxValue + 1)) - (int) (Math.random() * (maxValue + 1));
+        }
+        return arr;
+    }
+
+    // for test
+    public static void printArray(int[] arr) {
+        if (arr != null) {
+            for (int i = 0; i < arr.length; i++) {
+                System.out.print(arr[i] + " ");
+            }
+            System.out.println();
+        }
+    }
+
+    public static void main(String[] args) {
+        int maxLen = 10;
+        int maxValue = 20;
+        int testTime = 100000;
+        System.out.println("测试开始");
+        for (int i = 0; i < testTime; i++) {
+            int[] arr = generateRandomArray(maxLen, maxValue);
+            int sum = (int) (Math.random() * (maxValue + 1));
+            int ans1 = right(arr, sum);
+            int ans2 = num(arr, sum);
+            if (ans1 != ans2) {
+                System.out.println("Oops!");
+                printArray(arr);
+                System.out.println(sum);
+                System.out.println(ans1);
+                System.out.println(ans2);
+                break;
+            }
+        }
+        System.out.println("测试结束");
+
     }
 }
