a982744443257cc79d58155eacc1e68944d06c39
yibai <404223879@qq.com>
2023-11-16 19:26:30 +0800

    kmp

--- algorithmbasic/basicsets/class28/KMP.java
+++ algorithmbasic/basicsets/class28/KMP.java
@@ -14,13 +14,19 @@ public class KMP {
         int y = 0;
 
         while (x < str1.length && y < str2.length) {
+            /**
+             * 跳出循环分三种情况
+             * 1：当 x == str1.length && y != str2.length ==> 没有在str1中找到str2.
+             * 2：当 x == str1.length && y == str2.length ==> 在最后的时候正好找到了。.
+             * 3：当 x != str1.length && y == str2.length ==> 在前面就找到了.
+             */
             if (str1[x] == str2[y]) {
                 x++;
                 y++;
-            } else if (next[y] == -1) {
+            } else if (next[y] == -1) { // y == 0 ， 说明x所在的位置没有一个跟他匹配成功的，那就x++,继续向下找吧。
                 x++;
             } else {
-                y = next[y];
+                y = next[y]; // 直接来到next[y]位置与x位置进行比较。
             }
         }
         return y == str2.length ? x - y : -1;
@@ -38,9 +44,9 @@ public class KMP {
         while (i < str2.length) {
             if (str2[cn] == str2[i - 1]) {
                 next[i++] = ++cn;
-            } else if (cn > 0) {
+            } else if (cn > 0) { // 说明i-1位置是有最大相同字串的
                 cn = next[cn];
-            } else {
+            } else { // cn退到最开始的位置都不相等。说明当前i位置压根没有最大相同字串。
                 next[i++] = 0;
             }
         }
