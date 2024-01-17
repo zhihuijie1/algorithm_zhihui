7dc8eac59bd8e4aa27042e1aff8b5a2dfbf954c4
yibai <404223879@qq.com>
2024-01-17 22:44:41 +0800

    Morris前中遍历

--- algorithmbasic/basicsets/class31/MorrisTraversal.java
+++ algorithmbasic/basicsets/class31/MorrisTraversal.java
@@ -15,7 +15,7 @@ public class MorrisTraversal {
     public static void morris(Node head) {
         Node cur = head;
         Node mostright = null;
-        while(cur != null) {
+        while (cur != null) {
             if (cur.left == null) {
                 // 没有左孩子 -> 直接进入右孩子
                 cur = cur.right;
@@ -31,6 +31,81 @@ public class MorrisTraversal {
                     cur = cur.left;
                 } else {
                     mostright.right = null;
+                    cur = cur.right;
+                }
+            }
+        }
+    }
+
+    //morries的先序遍历
+    public static void morrisPre(Node head) {
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                System.out.print(cur.value + " ");
+                cur = cur.right;
+            } else {
+                mostRight = cur.left;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight == null) {
+                    mostRight.right = cur;
+                    System.out.print(cur.value + " ");
+                    cur = cur.left;
+                } else {
+                    mostRight = null;
+                    cur = cur.right;
+                }
+            }
+        }
+    }
+
+    // morries的中序遍历
+    public static void morrisIn(Node head) {
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                System.out.print(cur.value + " ");
+                cur = cur.right;
+            } else {
+                mostRight = cur.left;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                } else {
+                    mostRight = null;
+                    System.out.print(cur.value + " ");
+                    cur = cur.right;
+                }
+            }
+        }
+    }
+
+    // morries的后序遍历
+    public static void morrisLast(Node head) {
+        Node cur = head;
+        Node mostRight = null;
+        while (cur != null) {
+            if (cur.left == null) {
+                System.out.print(cur.value + " ");
+                cur = cur.right;
+            } else {
+                mostRight = cur.left;
+                while (mostRight.right != null && mostRight.right != cur) {
+                    mostRight = mostRight.right;
+                }
+                if (mostRight == null) {
+                    mostRight.right = cur;
+                    cur = cur.left;
+                } else {
+                    mostRight = null;
+                    System.out.print(cur.value + " ");
                     cur = cur.right;
                 }
             }
