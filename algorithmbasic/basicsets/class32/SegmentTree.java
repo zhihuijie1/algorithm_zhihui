f65157130f94e8df55223001a3496f464a8afc84
yibai <404223879@qq.com>
2024-01-26 02:06:48 +0800

    线段树

--- algorithmbasic/basicsets/class32/SegmentTree.java
+++ algorithmbasic/basicsets/class32/SegmentTree.java
@@ -1,135 +1,63 @@
 package algorithmbasic.basicsets.class32;
 
 public class SegmentTree {
+    /**
+     * 线段树主要解决的问题是：在一个区间上进行增，改，查，并且时间复杂度是logN
+     */
     public static class SegTree {
-        // arr[]为原序列的信息从0开始，但在arr里是从1开始的
-        // sum[]模拟线段树维护区间和
-        // lazy[]为累加和懒惰标记
-        // change[]为更新的值
-        // update[]为更新慵懒标记
+        //MAXN:传入数组的长度
+        //arr:从下标1开始复刻传入数组 原因：
+        //sum:模拟线段树维护区间和,从下标为1开始记录
+        //lazy:为累加和懒惰标记
+        //change:为更新的值
+        //update为更新慵懒标记 原因：如果change[x]==0，会出现歧义到底是这个区间要跟新为0还是这个区间不需要更新。所以引用update这个布尔类型进行标记到底需不需要更新。
         private int MAXN;
-        private int[] arr;
-        private int[] sum;
-        private int[] lazy;
-        private int[] change;
-        private boolean[] update;
+        private static int[] arr;
+        private static int[] sum;
+        private static int[] lazy;
+        private static int[] change;
+        private static boolean[] update;
 
-        public SegTree(int[] origin) {
-            MAXN = origin.length + 1;
-            arr = new int[MAXN]; // arr[0] 不用 从1开始使用
-            for (int i = 1; i < MAXN; i++) {
-                arr[i] = origin[i - 1];
+        public SegTree(int[] ans) {
+            MAXN = ans.length;
+            arr = new int[MAXN + 1];
+            sum = new int[MAXN << 2];
+            lazy = new int[MAXN << 2];
+            change = new int[MAXN << 2];
+            update = new boolean[MAXN << 2];
+            for (int i = 1; i < MAXN + 1; i++) {
+                arr[i] = ans[i - 1];
             }
-            sum = new int[MAXN << 2]; // 用来支持脑补概念中，某一个范围的累加和信息
-            lazy = new int[MAXN << 2]; // 用来支持脑补概念中，某一个范围沒有往下傳遞的纍加任務
-            change = new int[MAXN << 2]; // 用来支持脑补概念中，某一个范围有没有更新操作的任务
-            update = new boolean[MAXN << 2]; // 用来支持脑补概念中，某一个范围更新任务，更新成了什么
         }
 
-        private void pushUp(int rt) {
+        //在刚开始的时候要先构建sum数组(虚构的二叉树)原因：如果一开始就add/update其实sum这颗二叉树上是没有值的。所有的操作都是基于sum二叉树进行的
+        //lazy数组是辅助数组，主要是拦截的作用。
+        //
+        public static void build(int L, int R, int rt) {
+            if (L == R) {
+                sum[rt] = arr[L];
+                return;
+            }
+            int mid = (R + L) >> 1;
+            build(L, mid, rt << 1);
+            build(mid + 1, R, rt << 1 | 1);
             sum[rt] = sum[rt << 1] + sum[rt << 1 | 1];
         }
 
-        // 之前的，所有懒增加，和懒更新，从父范围，发给左右两个子范围
-        // 分发策略是什么
-        // ln表示左子树元素结点个数，rn表示右子树结点个数
-        private void pushDown(int rt, int ln, int rn) {
-            if (update[rt]) {
-                update[rt << 1] = true;
-                update[rt << 1 | 1] = true;
-                change[rt << 1] = change[rt];
-                change[rt << 1 | 1] = change[rt];
-                lazy[rt << 1] = 0;
-                lazy[rt << 1 | 1] = 0;
-                sum[rt << 1] = change[rt] * ln;
-                sum[rt << 1 | 1] = change[rt] * rn;
-                update[rt] = false;
-            }
-            if (lazy[rt] != 0) {
-                lazy[rt << 1] += lazy[rt];
-                sum[rt << 1] += lazy[rt] * ln;
-                lazy[rt << 1 | 1] += lazy[rt];
-                sum[rt << 1 | 1] += lazy[rt] * rn;
-                lazy[rt] = 0;
-            }
+        //任务：在[L,R]这个区间都加上C
+        //目前来到的区间是[l,r],sum对应的下标是rt
+        public static void add(int L, int R, int C, int l, int r, int rt) {
+
         }
 
-        // 在初始化阶段，先把sum数组，填好
-        // 在arr[l~r]范围上，去build，1~N，
-        // rt : 这个范围在sum中的下标
-        public void build(int l, int r, int rt) {
-            if (l == r) {
-                sum[rt] = arr[l];
-                return;
-            }
-            int mid = (l + r) >> 1;
-            build(l, mid, rt << 1);
-            build(mid + 1, r, rt << 1 | 1);
-            pushUp(rt);
+        public static void update() {
+
         }
 
+        public static void quary() {
 
-        // L~R  所有的值变成C
-        // l~r  rt
-        public void update(int L, int R, int C, int l, int r, int rt) {
-            if (L <= l && r <= R) {
-                update[rt] = true;
-                change[rt] = C;
-                sum[rt] = C * (r - l + 1);
-                lazy[rt] = 0;
-                return;
-            }
-            // 当前任务躲不掉，无法懒更新，要往下发
-            int mid = (l + r) >> 1;
-            pushDown(rt, mid - l + 1, r - mid);
-            if (L <= mid) {
-                update(L, R, C, l, mid, rt << 1);
-            }
-            if (R > mid) {
-                update(L, R, C, mid + 1, r, rt << 1 | 1);
-            }
-            pushUp(rt);
         }
-
-        // L~R, C 任务！
-        // rt，l~r
-        public void add(int L, int R, int C, int l, int r, int rt) {
-            // 任务如果把此时的范围全包了！
-            if (L <= l && r <= R) {
-                sum[rt] += C * (r - l + 1);
-                lazy[rt] += C;
-                return;
-            }
-            // 任务没有把你全包！
-            // l  r  mid = (l+r)/2
-            int mid = (l + r) >> 1;
-            pushDown(rt, mid - l + 1, r - mid);
-            // L~R
-            if (L <= mid) {
-                add(L, R, C, l, mid, rt << 1);
-            }
-            if (R > mid) {
-                add(L, R, C, mid + 1, r, rt << 1 | 1);
-            }
-            pushUp(rt);
-        }
-
-        // 1~6 累加和是多少？ 1~8 rt
-        public long query(int L, int R, int l, int r, int rt) {
-            if (L <= l && r <= R) {
-                return sum[rt];
-            }
-            int mid = (l + r) >> 1;
-            pushDown(rt, mid - l + 1, r - mid);
-            long ans = 0;
-            if (L <= mid) {
-                ans += query(L, R, l, mid, rt << 1);
-            }
-            if (R > mid) {
-                ans += query(L, R, mid + 1, r, rt << 1 | 1);
-            }
-            return ans;
-        }
-
     }
 }
+
+//rt << 1 | 1
