e8cdd9ca46ca965fa05eebca315db3f2a79e8733
yibai <404223879@qq.com>
2024-02-05 23:01:20 +0800

    indexTree

--- algorithmbasic/basicsets/class33/IndexTree2D.java
+++ algorithmbasic/basicsets/class33/IndexTree2D.java
@@ -3,21 +3,36 @@ package algorithmbasic.basicsets.class33;
 public class IndexTree2D {
     // 二维indexTree
     // 注意必须从1下标开始，才匹配后面的规律
-    //  1  2  3  4  5  6  7  8  9  10  11
-    //1 *  *  *  *  *  *  *  *  *   *  *
-    //2 *  *  *  *  *  *  *  *  *   *  *
-    //3 *  *  *  *  *  *  *  *  *   *  *
-    //4 *  *  *  *  *  *  *  *  *   *  *
-    //5 *  *  *  *  *  *  *  *  *   *  *
-    //6 *  *  *  *  *  *  *  *  *   *  *
-    //7 *  *  *  *  *  *  *  *  *   *  *
-    //8 *  *  *  *  *  *  *  *  *   *  *
-    //9 *  *  *  *  *  *  *  *  *   *  *
-    //10*  *  *  *  *  *  *  *  *   *  *
-    //11*  *  *  *  *  *  *  *  *   *  *
+    //   1  2  3  4  5  6  7  8  9  10  11
+    // 1 *  *  *  *  *  *  *  *  *   *  *
+    // 2 *  *  *  *  *  *  *  *  *   *  *
+    // 3 *  *  *  *  *  *  *  *  *   *  *
+    // 4 *  *  *  *  *  *  *  *  *   *  *
+    // 5 *  *  *  *  *  *  *  *  *   *  *
+    // 6 *  *  *  *  *  *  *  *  *   *  *
+    // 7 *  *  *  *  *  *  *  *  *   *  *
+    // 8 *  *  *  *  *  *  *  *  *   *  *
+    // 9 *  *  *  *  *  *  *  *  *   *  *
+    // 10*  *  *  *  *  *  *  *  *   *  *
+    // 11*  *  *  *  *  *  *  *  *   *  *
 
     //计算区间的累加和
     //更新某点的值
 
-
+    // Colorado State University (Scott)
+    // Clemson University
+    // Boise State University
+    // Binghamton University--SUNY (Watson)
+    // Columbia University (Fu Foundation)
+    // Drexel University
+    // Florida Atlantic University
+    // Florida Institute of Technology
+    // George Mason University
+    // George Washington University
+    // Illinois Institute of Technology (Armour)
+    // Kansas State University
+    // Lehigh University (Rossin)
+    // Louisiana Tech University
+    // Marquette University (Opus)
+    // Michigan Technological University
 }
